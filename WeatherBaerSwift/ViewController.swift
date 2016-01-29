bd7833f6cd2160db7ba121d68c6888ca069dea8f
Ken Baer <ken.baer@cambiahealth.com>
2016-01-28 20:55:14 -0800

    Initial Commit

---
+++ WeatherBaerSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherBaerSwift
+//
+//  Created by Ken Baer on 1/28/16.
+//  Copyright © 2016 BaerCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
