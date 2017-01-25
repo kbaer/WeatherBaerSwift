c897d4a01b5eef816c3a5fc8f9a7f123e683130b
Ken Baer <kenbaer@me.com>
2017-01-25 13:52:51 -0800

    Cleanup.

---
+++ Pods/Target Support Files/ForecastIOClient/ForecastIOClient-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double ForecastIOClientVersionNumber;
+FOUNDATION_EXPORT const unsigned char ForecastIOClientVersionString[];
+
